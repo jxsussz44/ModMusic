a933aca1a69aa3d3af9400ec6cb590b8bda4701f
garciasamuel0412 <garciasamuel10412@gmailcom>
2026-04-24 17:07:12 -0500

    zzz

--- ventanas/css/index.js
+++ ventanas/css/index.js
@@ -1,43 +1,14 @@
-/* LOGIN */
-function openLogin(){
-document.getElementById("loginModal").style.display="flex";
+Controller.filter = function(type){
+if(type==="all") return View.renderProducts(Model.products);
+
+const map={
+cuerda:["Guitarra","Bajo","Violín"],
+viento:["Saxofón"]
+};
+
+View.renderProducts(
+Model.products.filter(p=>map[type]?.includes(p.name))
+);
 }
-
-function login(){
-alert("Bienvenido a MOD MUSIC 🔥");
-document.getElementById("loginModal").style.display="none";
-}
-
-/* CHECKOUT */
-function openCheckout(){
-const box=document.getElementById("checkout");
-const items=document.getElementById("checkoutItems");
-
-items.innerHTML="";
-let total=0;
-
-cart.forEach(p=>{
-items.innerHTML+=`<p>${p.name} x ${p.qty}</p>`;
-total+=p.price*p.qty;
-});
-
-document.getElementById("checkoutTotal").innerText="Total: $"+total;
-
-box.classList.add("active");
-}
-
-/* PAGAR */
-function pay(){
-alert("Pago exitoso (simulado) 💳");
-cart=[];
-save();
-updateCart();
-document.getElementById("checkout").classList.remove("active");
-}
-
-/* BOTON EN CARRITO */
-const btn=document.createElement("button");
-btn.innerText="Finalizar compra";
-btn.onclick=openCheckout;
-
-document.getElementById("cart").appendChild(btn);
+document.getElementById("notif").style.display="block";
+setTimeout(()=>document.getElementById("notif").style.display="none",1500);
